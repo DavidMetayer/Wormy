2041094224516791599e3d17e9fcfaef3a2f848f
c0641903 <c0641903@SB-N208-19.lambton.on.ca>
2016-03-14 15:28:51 -0400

    Worm should be good.

--- src/main/java/model/Worm.java
+++ src/main/java/model/Worm.java
@@ -13,31 +13,64 @@ import java.util.List;
  * @author c0641903
  */
 public class Worm {
+    private boolean alive;
+    private int movement;
     private int position;
+    private int prevEndPos;
     private List<Integer> coords = new ArrayList<>();
     
-    public int moveLeft(){
-        if (coords.get(0) % 50 != 0){
-            position = coords.get(0) - 1;
+    public void setInitialCoords(int c1, int c2, int c3){
+        coords.add(c1);
+        coords.add(c2);
+        coords.add(c3);
+    }
+    public String getPosition(){
+        return "g" + position;
+    }
+    public void moveUp(){
+        movement = -50;
+    }
+    public void moveRight(){
+        movement = 1;
+    }
+    public void moveDown(){
+        movement = 50;
+    }
+    public void moveLeft(){
+        movement = -1;
+    }
+    public void move(){
+        String positionString = Integer.toString(position);
+        // May cause an error.
+        if (position < 50 && movement == -50){
+            alive = false;
+        } else if ((positionString.endsWith("49") || positionString.endsWith("99")) && movement == 1){
+            alive = false;
+        } else if (position > 2449 && movement == 50){
+            alive = false;
+        } else if (position % 50 == 0 && movement == -1){
+            alive = false;
         } else {
-            position = coords.get(0);
+            int coordA = coords.get(0);
+            int coordB;
+            position += movement;
+            coords.set(0, position);
+            // May cause an error.
+            for (int a = 1; a < coords.size()-1; a++){
+                coordB = coords.get(a);
+                coords.set(a, coordA);
+                coordA = coordB;
+            }
+            prevEndPos = coordA;
         }
-      //  return getPosition(); // Don't worry about this right now. Just planning the logic.
-        return 0;
     }
-    
-    public int moveRight(){
-        String positionString = Integer.toString(coords.get(0));
-        if(positionString.endsWith("49") != true || positionString.endsWith("99") != true){
-            position = coords.get(0) + 1;
-        }
-        else{
-            position = coords.get(0);
-        }
-        
-        //return getPosition(); // Don't worry about this right now. Just planning the logic.
-        
-        return 0;
+    public String getCoord(int index){
+        return "g" + coords.get(index);
     }
-    
+    public void grow(){
+        coords.add(prevEndPos);
+    }
+    public boolean getStatus(){
+        return alive;
+    }
 }
